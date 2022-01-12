bc0cfb3572fba7c28d2ebe8640cf05ba3382b1b9
nicowickersheim <nico.wickersheim@yahoo.de>
2022-01-13 00:47:36 +0100

    added Changelog page

---
+++ TimetrackerForProjects/Pages/Changelog.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TimetrackerForProjects.Pages
+{
+    public class ChangelogModel : PageModel
+    {
+
+        private readonly ILogger<ChangelogModel> _logger;
+
+        public ChangelogModel(ILogger<ChangelogModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- TimetrackerForProjects/wwwroot/js/site.js
+++ TimetrackerForProjects/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(function () {
+    // executes when HTML-Document is loaded and DOM is ready
+    console.log("document is ready");
+
+
+    $(".card").hover(
+        function () {
+            $(this).addClass('shadow-lg').css('cursor', 'pointer');
+        }, function () {
+            $(this).removeClass('shadow-lg');
+        }
+    );
+
+    // document ready  
+});
+
+
